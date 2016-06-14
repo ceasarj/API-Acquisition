f0f54dd7209d6b86890ed605b6398597bd8894d9
ceejay <cvvjimenez@gmail.com>
2016-06-14 04:18:14 -0700

    Avoid OutOfMemory exception

--- src/main/java/acquisition/YoutubeSource.java
+++ src/main/java/acquisition/YoutubeSource.java
@@ -6,18 +6,17 @@ import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
-import model.VideoModel;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
+import com.google.api.services.youtube.model.VideoStatistics;
+import model.VideoModel;
 
 public class YoutubeSource implements Source<VideoModel> {
 
     private YouTube youtube;
-    private List<VideoModel> videos;
+    private Stack<List<VideoModel>> videos;
     private String query;
     private String pageToken;
 
@@ -25,7 +24,7 @@ public class YoutubeSource implements Source<VideoModel> {
 
     public YoutubeSource(String query){
         this.query = query;
-        this.videos = new ArrayList<>();
+        this.videos = new Stack<>();
         this.pageToken = "";
 
         this.youtube = new YouTube.Builder(
@@ -36,16 +35,17 @@ public class YoutubeSource implements Source<VideoModel> {
 
     @Override
     public boolean hasNext() {
-        return false;
+        search();
+        return !videos.empty();
     }
 
     @Override
     public Collection<VideoModel> next() {
-        return null;
+        return videos.pop();
     }
 
     private void search() {
-        while(pageToken != null) {
+        List<VideoModel> videoModels = new ArrayList<>();
             try {
                 // set up the search url
                 YouTube.Search.List search = youtube.search()
@@ -73,12 +73,33 @@ public class YoutubeSource implements Source<VideoModel> {
 
                     // check for only youtube videos
                     if (rId.getKind().equals("youtube#video")) {
-                        System.out.println(video.getSnippet().getTitle());
+                        videoModels.add(getVideoModel(video));
                     }
                 }
+
+                if(videoModels.size() > 0)
+                    videos.push(videoModels);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
+    }
+
+    private VideoModel getVideoModel(SearchResult video) {
+        // set up the video model
+        VideoModel vm = new VideoModel();
+        vm.id = video.getId().getVideoId();
+        vm.title = video.getSnippet().getTitle();
+        vm.publishedDate = video.getSnippet().getPublishedAt().toString();
+        vm.word = query;
+        return vm;
+    }
+
+    public static void main(String[] args){
+        YoutubeSource ys = new YoutubeSource("Batman");
+        while(ys.hasNext()){
+            ArrayList<VideoModel> vm = (ArrayList)ys.next();
+            System.out.println(vm.get(0).title);
         }
     }
 }
